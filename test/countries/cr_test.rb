4f22f90acb814b4f06e74ccf3ec16f2c88ea2dab
Conor Hynes <conor.hynes.personal@gmail.com>
2016-09-06 12:31:03 +0100

    Costa rica fix

--- test/countries/cr_test.rb
+++ test/countries/cr_test.rb
@@ -3,10 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 ## Costa Rica
 class CRTest < Phonie::TestCase
   def test_local
-    parse_test('+50621234567', '506', '2', '1234567', 'Costa Rica', false)
+    parse_test('+50622123456', '506', '22', '123456', 'Costa Rica', false)
   end
 
   def test_mobile
-    parse_test('50681234567', '506', '8', '1234567', 'Costa Rica', true)
+    parse_test('50683123456', '506', '83', '123456', 'Costa Rica', true)
   end
 end
